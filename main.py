35782d2ddc07a0b937c76837ade840692e88ee6c
Over-haven <tanushdas26@gmail.com>
2024-10-13 21:02:33 +0530

    Hey

---
+++ main.py
@@ -0,0 +1,13 @@
+#creating different varibles
+x = 5
+y = "Tanush"
+print(x)
+print(y)
+
+codingal = "codingal"
+print("teacher")
+
+#user input
+name = input("enter your name :")
+
+print("\nHello",name,"welcome to codingal\n\n")
